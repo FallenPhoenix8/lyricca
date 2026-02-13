2f9d7d6235769e657e03ccd3482f21f1e29b0cb2
FallenPhoenix8 <tplayxyz8@gmail.com>
2026-02-13 17:49:14 +0100

    Implement `User` type validation

---
+++ apps/web/lib/model/User.ts
@@ -0,0 +1,30 @@
+import type { UserDTO, UserCreate, UserUpdate } from "@shared/ts-types/user-dto"
+import { type } from "arktype"
+
+const UserSchema = type({
+  id: "string.uuid",
+  username: "string > 0",
+  created_at: "Date",
+  updated_at: "Date",
+})
+const TUserDTO = UserSchema.infer satisfies UserDTO
+
+const UserCreateSchema = type({
+  username: "string > 0",
+  /**
+   * Rules:
+   * - min 8 chars,
+   * - at least 1 lowercase,
+   * - 1 uppercase,
+   * - 1 digit,
+   * - 1 symbol
+   * */
+  password: "/^(?=.{8,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).*$/",
+})
+const TUserCreate = UserCreateSchema.infer satisfies UserCreate
+
+const UserUpateSchema = UserCreateSchema.partial()
+const TUserUpdate = UserUpateSchema.infer satisfies UserUpdate
+
+export { TUserDTO, TUserCreate, TUserUpdate }
+export { UserSchema, UserCreateSchema, UserUpateSchema }
