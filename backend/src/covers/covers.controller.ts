c1135019d71ce9f8b9bf5c4bde975677db6dcf91
FallenPhoenix8 <tplayxyz8@gmail.com>
2026-03-14 23:36:44 +0100

    fix: add bad request exception when user gives empty query parameters for /covers/suggestion

--- backend/src/covers/covers.controller.ts
+++ backend/src/covers/covers.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Controller,
   Get,
   HttpStatus,
@@ -19,13 +20,30 @@ export class CoversController {
   @Get("suggestion")
   async getSuggestion(
     @Req() req: AuthenticatedRequest,
-    @Query("artist") artist: string,
-    @Query("title") title: string,
+    @Query("artist") artist: string | undefined,
+    @Query("title") title: string | undefined,
   ): Promise<SuggestionDTO> {
+    const errors: string[] = []
+    if (!artist?.trim()) {
+      errors.push(
+        "`artist` query parameter is required. It must be a non-empty string.",
+      )
+    }
+
+    if (!title?.trim()) {
+      errors.push(
+        "`title` query parameter is required. It must be a non-empty string.",
+      )
+    }
+
+    if (errors.length > 0) {
+      throw new BadRequestException(errors)
+    }
+
     const userAgent = req.headers["user-agent"] ?? "LyriccaApp/1.0"
     const coverURL = await this.coversService.getSuggestionURL({
-      artist,
-      title,
+      artist: artist!,
+      title: title!,
       userAgent,
     })
 
